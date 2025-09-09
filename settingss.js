a247eb0e6913db77d45089aa26ae22c1c4632e71
yoby479 <yobimnare@gmail.com>
2025-09-09 15:29:06 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,62 @@
+{
+  "name": "ᴍᴀʟᴠɪɴ-xᴅ",
+  "description": "Javascript WhatsApp bot made by ᴍᴀʟᴠɪɴ ᴋɪɴɢ",
+  "logo": "https://files.catbox.moe/qumhu4.jpg",
+  "keywords": ["MALVIN XD"],
+  "success_url": "/",
 
-module.exports = {
-  SESSION_ID: "malvin~BnNV0ToB#E0JVOthvWeTAZQkKn0iMSpcKbbfAYAzRiIynGvM3nYs",  // add your Session Id here
+    "stack": "container",
+  "env": {
+      "SESSION_ID": {
+        "description": "malvin~BnNV0ToB#E0JVOthvWeTAZQkKn0iMSpcKbbfAYAzRiIynGvM3nYs",
+        "value":"",
+        "required": true
+        
+    },
+      "BOT_NAME": {
+        "description": "JUNIOR SIRMA.",
+        "required": true,
+        "value": "ᴍᴀʟᴠɪɴ xᴅ" 
+        
+    },
+     "PREFIX": {
+       "description": "Bot command prefix, (e.g, . , ! @ etc",
+      "value": "."
+      "required": true,
+      
+    },
+      "MODE": {
+        "description": "Bot mode can be public, private, inbox or groups in small letters (e.g, public)",
+        "value":"private",
+        "required": true  
+        
+   },
+     "DESCRIPTION": {
+      "description": "add caption for menu and other",
+      "value": "> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍᴀʟᴠɪɴ xᴅ*",
+      "required": true
+      
+   }, 
+
+    "OWNER_NUMBER": {
+      "description": "put the owner number for bot.",
+      "value": "254798457039"
+      "required": true
+      
+   },
+    "OWNER_NAME": {
+      "description": "Enter your name here",
+      "value": "",
+      "required": true
+    }
+
+},
+
+     "buildpacks": [
+        {
+            "url": "https://github.com/heroku/heroku-buildpack-nodejs.git"
+        }
+     ],
+  "stack": "heroku-24"
+}
   
-  OWNER_NUMBER: "254116772320", // put your phone number here
-  
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
-  
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
-};
