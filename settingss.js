e04e5401ff3fc8180dff3c8aa24478ccbea41581
yoby479 <yobimnare@gmail.com>
2025-09-10 01:10:51 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -3,7 +3,7 @@ module.exports = {
   
   OWNER_NUMBER: "254116772320", // put your phone number here
   
-  PREFIX: "🖕", // prefix (e.g., ., /, !, *)
+  PREFIX: ".", // prefix (e.g., ., /, !, *)
   
   TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
 };
