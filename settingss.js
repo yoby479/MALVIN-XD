6d9473401dd6e8c40d6d562466c07c398c6b3c1e
yoby479 <yobimnare@gmail.com>
2025-09-14 12:40:07 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,7 +1,7 @@
 module.exports = {
-  SESSION_ID: "malvin~om8lEIrS#MlAfcSCAp1jvz6Mi8_R05QLVTkRLuxurOyRJPDdhJhk",  // add your Session Id here
+  SESSION_ID: "malvin~1jMDXbiD#UgT5Kx_Q8mRBt9Uz8XkiQHcJfljRhiHF5Sadck3ktT0",  // add your Session Id here
   
-  OWNER_NUMBER: "254710637953", // put your phone number here
+  OWNER_NUMBER: "254723413244", // put your phone number here
   
   PREFIX: ",", // prefix (e.g., ., /, !, *)
   
